dc685bd6c3f60b7eeb595e90ec1c5869172c7cda
Santiago Segurado <santisegurado1@gmail.com>
2023-09-30 09:09:04 -0300

    Footer agregado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Calculator from "@/components/Calculator";
 import Contact from "@/components/Contact";
 import ExpertiseFileds from "@/components/ExpertiseFileds";
 import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeaderResposive from "@/components/HeaderResposive";
 
@@ -19,6 +20,7 @@ export default function Home() {
       <About/>
       <Calculator/>
       <Contact/>
+      <Footer/>
       <a href="#home">
         <div className="fixed bottom-5 right-5 cursor-pointer z-50 bg-[#BF9874] rounded-sm">
           <img src="/arrow-top.svg" alt="" />
