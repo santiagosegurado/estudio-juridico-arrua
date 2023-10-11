d1caf856e3e32fe17140d2551fa51361d2636627
Santiago Segurado <santisegurado1@gmail.com>
2023-10-11 17:27:30 -0300

    Info Contacto agregado

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -36,19 +36,18 @@ const Contact = () => {
     });
 
     if (data.id) {
-      toast.success("Mensaje enviado con éxito")
-    }else{
-      toast.error("Error al enviar el mensaje")
+      toast.success("Mensaje enviado con éxito");
+    } else {
+      toast.error("Error al enviar el mensaje");
     }
-    
   };
 
   return (
     <>
-      <Toaster richColors/>
+      <Toaster richColors />
       <section
         id="contacto"
-        className="flex flex-col py-28 gap-10 bg-[#001025]"
+        className="flex flex-col py-28 gap-14 bg-[#001025]"
       >
         <div className="flex flex-col items-center">
           <div className="flex flex-row gap-7 items-center">
@@ -80,8 +79,65 @@ const Contact = () => {
             </span>
           </div>
         </div>
-        <div className="flex flex-col gap-8 max-w-5xl self-center w-full">
-          <form className="flex flex-col gap-5 px-2" onSubmit={handleSubmit}>
+        <div className="flex flex-col-reverse md:flex-row gap-8 justify-center ">
+          <div className="md:w-[40%]">
+            <div className="flex flex-row gap-8 p-4 flex-wrap">
+              <div className="flex flex-row gap-3">
+                <div className="w-[20px] h-[20px]">
+                  <img
+                    className="h-full w-full"
+                    src="/phone.svg"
+                    alt="Estudio Arrua"
+                  />
+                </div>
+                <span className="text-slate-500 font-light">1168761237</span>
+              </div>
+              <div className="flex flex-row gap-3">
+                <div className="w-[20px] h-[20px]">
+                  <img
+                    className="h-full w-full"
+                    src="/clock.svg"
+                    alt="Estudio Arrua"
+                  />
+                </div>
+
+                <span className="text-slate-500 font-light">
+                  LUNES - VIERNES de 8:00 a 18:00
+                </span>
+              </div>
+              <div className="flex flex-row gap-3">
+                <div className="w-[23px] h-[23px]">
+                  <img
+                    className="h-full w-full"
+                    src="/location.svg"
+                    alt="Estudio Arrua"
+                  />
+                </div>
+                <span className="text-slate-500 font-light">
+                  Entre Ríos 2942, San Justo
+                </span>
+              </div>
+              <div className="flex flex-row gap-3">
+                <div className="w-[23px] h-[23px]">
+                  <img
+                    className="h-full w-full"
+                    src="/email.svg"
+                    alt="Estudio Arrua"
+                  />
+                </div>
+                <span className="text-slate-500 font-light">
+                  arrua.abogados@gmail.com
+                </span>
+              </div>
+              <div className="w-full">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.1760345721186!2d-58.56347022363452!3d-34.67550637292873!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc62ec4fc7cd9%3A0x72915a508dde7f12!2sAVZ%2C%20Entre%20R%C3%ADos%202942%2C%20B1754%20San%20Justo%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1697053256359!5m2!1ses!2sar" width="100%" height="200" style={{border:0}} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+              </div>
+            </div>
+          </div>
+          <form
+            className="flex flex-col gap-5 px-2 md:w-[50%] "
+            onSubmit={handleSubmit}
+          >
             <div className="flex flex-col md:flex-row items-center gap-5">
               <input
                 type="text"
@@ -125,6 +181,7 @@ const Contact = () => {
                 }
                 value={contact.message}
                 required
+                rows={6}
               ></textarea>
             </div>
             <input
